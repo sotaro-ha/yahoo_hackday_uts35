2bc76229b495b81e36eaff65b8d5095d5380c85b
横井総太朗 <alphacomplex.104729@gmail.com>
2021-09-25 19:27:32 +0900

    set up vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="hey to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/mainPage.vue'
 
 export default {
   name: 'App',
