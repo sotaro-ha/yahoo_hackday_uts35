aa7834ae111acee40350e1f6550401ebb8e5b84f
横井総太朗 <alphacomplex.104729@gmail.com>
2021-09-25 20:53:19 +0900

    create main page

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -1,32 +1,21 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="app">
+   <header>
+     <div class="header_container">
+        <div class="header_logo"><img class="logo_img" src="../assets/logo.png" width="50px"/></div>
+        <button class="my_page">My Page</button>
+     </div>
+   </header>
+   <div class="content_container">
+   <div class="content_panel"><div class="shop_image"></div><div class="shop_text"><p class="shop_title">コンビニ</p></div></div>
+   <div class="content_panel"><div class="shop_image"></div><div class="shop_text"></div></div>
+   <div class="content_panel"><div class="shop_image"></div><div class="shop_text"></div></div>
+   <div class="content_panel"><div class="shop_image"></div><div class="shop_text"></div></div>
+   <div class="content_panel"><div class="shop_image"></div><div class="shop_text"></div></div>
+   <div class="content_panel"><div class="shop_image"></div><div class="shop_text"></div></div>
+   <div class="content_panel"><div class="shop_image"></div><div class="shop_text"></div></div>
+   <div class="content_panel"><div class="shop_image"></div><div class="shop_text"></div></div>
+   </div>
   </div>
 </template>
 
@@ -41,19 +30,73 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.app{
+  transition: all 1s ease-in;
+}
+header{
+  height :70px;
+  background-color: rgb(255, 221, 110);
+  width:100%;
+}
+.header_container{
+  max-width:1200px;
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+   padding:10px;
+}
+.my_page{
+  width:100px;
+  height:40px;
+  background:#ffffff;
+  border-radius:25px;
+  font-size:16px;
+  border:none;
+  cursor:pointer;
+}
+.my_page:hover{
+  filter:drop-shadow(0px 0px 5px rgba(0,0,0,0.1));
+  transition: all .1s ease-in;
+}
+.content_container{
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap:wrap;
+  padding:10px;
+}
+.content_panel{
+  width:200px;
+  height:300px;
+  background-color:white;
+  margin-bottom:16px;
+  margin-right:16px;
+  border-radius: 0 0 10px 10px; 
+filter:drop-shadow(0px 0px 3px rgba(0,0,0,0.3));
+}
+.content_panel:hover{
+  filter:drop-shadow(0px 0px 5px rgba(0,0,0,0.5));
+  transition: all .1s ease-in;
+}
+.shop_image{
+  background-color:white;
+  background-image:url("../assets/onigiri_maru.png");
+  background-size:contain;
+  background-repeat: no-repeat;
+  width:200px;
+  height:200px;
+  filter:drop-shadow(0px 3px 3px rgba(0,0,0,0.2));
+}
+.shop_text{
+  height:100px;
+  background-color:orange;
+  border-radius: 0 0 10px 10px; 
+}
+.shop_title{
+  font-size:18px;
+  text-align:left;
+  font-weight:700;
+  margin-left:16px;
+}
 
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
