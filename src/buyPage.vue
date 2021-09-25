9090dc1e3e3b1ee554c1026dec41cbe2a3e3030b
横井総太朗 <alphacomplex.104729@gmail.com>
2021-09-25 14:02:19 +0900

    add page and add package-rooter

---
+++ src/buyPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div :class="$style.parent">
+    <div :class="$style.child">
+      <h1>Routing Test Page</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Routing'
+}
+</script>
